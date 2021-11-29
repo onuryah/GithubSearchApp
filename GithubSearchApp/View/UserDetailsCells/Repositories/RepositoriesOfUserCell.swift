c5b7d7588a7c239ada937616e1616e327b2d5d89
Onuralp <xonuraralpx@gmail.com>
2021-11-29 22:18:56 +0300

    first commit

---
+++ GithubSearchApp/View/UserDetailsCells/Repositories/RepositoriesOfUserCell.swift
@@ -0,0 +1,23 @@
+//
+//  RepositoriesOfUserCell.swift
+//  GithubSearchApp
+//
+//  Created by Ceren Çapar on 29.11.2021.
+//
+
+import UIKit
+
+class RepositoriesOfUserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
